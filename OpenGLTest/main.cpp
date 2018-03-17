06e004d0ef95c846081314d0441337bf2c2b600e
Zcating <zcating@Qq.com>
2018-03-17 12:39:52 +0800

    Session Texture addition.

--- OpenGLTest/main.cpp
+++ OpenGLTest/main.cpp
@@ -18,6 +18,7 @@
 
 // Other includes
 #include "Shader.hpp"
+#include "GLTexture.hpp"
 
 
 // Function prototypes
@@ -25,6 +26,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
+float mixValue = 0;
 
 // The MAIN function, from here we start the application and run the game loop
 int main()
@@ -57,7 +59,7 @@ int main()
     
     
     // Build and compile our shader program
-    Shader ourShader("/Users/zcating/Project/iOSCode/OpenGLTest/OpenGLTest/triangle.vsh", "/Users/zcating/Project/iOSCode/OpenGLTest/OpenGLTest/triangle.fsh");
+    Shader ourShader("/Users/zcating/Project/MyGithub/OpenGLTest/OpenGLTest/triangle.vsh", "/Users/zcating/Project/MyGithub/OpenGLTest/OpenGLTest/triangle.fsh");
     
     
     // Set up vertex data (and buffer(s)) and attribute pointers
@@ -98,49 +100,12 @@ int main()
     glBindVertexArray(0); // Unbind VAO
     
     
-    // Load and create a texture
-    GLuint texture1;
-    GLuint texture2;
-    // ====================
-    // Texture 1
-    // ====================
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1); // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
-    // Set our texture parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);    // Set texture wrapping to GL_REPEAT
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // Set texture filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // Load, create texture and generate mipmaps
-    int width, height;
-    unsigned char* image = SOIL_load_image("/Users/zcating/Project/iOSCode/OpenGLTest/container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    SOIL_free_image_data(image);
-    glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
-    // ===================
-    // Texture 2
-    // ===================
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    // Set our texture parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // Set texture filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // Load, create texture and generate mipmaps
-    image = SOIL_load_image("/Users/zcating/Project/iOSCode/OpenGLTest/awesomeface.png", &width, &height, 0, SOIL_LOAD_RGB);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    SOIL_free_image_data(image);
-    glBindTexture(GL_TEXTURE_2D, 0);
+    GLTexture boxTexture = GLTexture("/Users/zcating/Project/MyGithub/OpenGLTest/container.jpg");
+    GLTexture smileTexture = GLTexture("/Users/zcating/Project/MyGithub/OpenGLTest/awesomeface.png");
     
     
     // Game loop
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
         glfwPollEvents();
         
@@ -154,12 +119,14 @@ int main()
         
         // Bind Textures using texture units
         glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glUniform1i(glGetUniformLocation(ourShader.program, "boxTexture"), 0);
+        glBindTexture(GL_TEXTURE_2D, boxTexture.getPointer());
+
         glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
-        glUniform1i(glGetUniformLocation(ourShader.program, "smileTexture"), 1);
+        glBindTexture(GL_TEXTURE_2D, smileTexture.getPointer());
         
+        ourShader.setInt("boxTexture", 0);
+        ourShader.setInt("smileTexture", 1);
+        ourShader.setFloat("mixValue", mixValue);
         // Draw container
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -178,9 +145,15 @@ int main()
 }
 
 // Is called whenever a key is pressed/released via GLFW
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, GL_TRUE);
+    } else if (key == GLFW_KEY_UP) {
+        mixValue += 0.01;
+    } else if (key == GLFW_KEY_DOWN && mixValue > 0){
+        mixValue -= 0.01;
+    } else if (mode == GLFW_MOD_SUPER && key == GLFW_KEY_W) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
 }
 
