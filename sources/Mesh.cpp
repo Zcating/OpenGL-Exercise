a11fc6ceb73cec8a571925e9dcc9449f6a65c35d
sunhay.zhu <zcatstring@gmail.com>
2018-04-09 18:29:39 +0800

    Session Model Load - Mesh

---
+++ sources/Mesh.cpp
@@ -0,0 +1,45 @@
+//
+//  Mesh.cpp
+//  OpenGLTest
+//
+//  Created by  zcating on 2018/4/9.
+//  Copyright © 2018 com.zcating. All rights reserved.
+//
+
+#include "Mesh.hpp"
+
+using namespace std;
+
+Mesh::Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+{
+    this->vertices = vertices;
+    this->indices = indices;
+    this->textures = textures;
+    _setupMesh();
+}
+
+void Mesh::_setupMesh()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    
+    glBindVertexArray(VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
+    
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, normal));
+    
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, textureCoordinates));
+    
+    glBindVertexArray(0);
+    
+}
---
+++ sources/Mesh.hpp
@@ -0,0 +1,46 @@
+//
+//  Mesh.hpp
+//  OpenGLTest
+//
+//  Created by  zcating on 2018/4/9.
+//  Copyright © 2018 com.zcating. All rights reserved.
+//
+
+#ifndef Mesh_hpp
+#define Mesh_hpp
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <vector>
+
+#include "Shader.hpp"
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 textureCoordinates;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+};
+
+class Mesh {
+public:
+    /*  网格数据  */
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+    /*  函数  */
+    Mesh(const std::vector<Vertex>& vertices,const std::vector<unsigned int>& indices,const std::vector<Texture>& textures);
+    void draw(const Shader& shader);
+private:
+    /*  渲染数据  */
+    unsigned int VAO, VBO, EBO;
+    /*  函数  */
+    void _setupMesh();
+};
+
+#endif /* Mesh_hpp */
